5928eaa4b21d465baa88a445760f585651eca67b
Giovanni Catania <giovanni.catania@ynap.com>
2020-12-06 13:04:10 +0100

    First commit

---
+++ Pokedex-SwiftUI/Pokedex-SwiftUI/Pokedex_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokedex_SwiftUIApp.swift
+//  Pokedex-SwiftUI
+//
+//  Created by Giovanni Catania on 06/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
